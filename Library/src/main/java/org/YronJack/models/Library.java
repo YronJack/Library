40523a1ddc18c9c699a1d0d284fab2452d4cae74
afminguela <afminguela@gmail.com>
2025-07-25 14:55:09 +0200

    book class created and done, library and persona abstract class created

---
+++ Library/src/main/java/org/YronJack/models/Library.java
@@ -0,0 +1,13 @@
+package org.YronJack.models;
+
+import org.YronJack.models.*;
+import java.util.*;
+
+public class Library {
+       private List<Book> booksList;
+       private List<Students> studentsList;
+       private List<Issues> issuesList;
+       private List<Authors> authorsList;
+
+
+}
