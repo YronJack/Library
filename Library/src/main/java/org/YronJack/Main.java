b673bd7eea795b7c55ce46608f8e43fb6a8909c7
afminguela <afminguela@gmail.com>
2025-07-25 11:49:48 +0200

    third commit on local

--- Library/src/main/java/org/YronJack/Main.java
+++ Library/src/main/java/org/YronJack/Main.java
@@ -1,13 +1,11 @@
 package org.YronJack;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        String out;
+
+        System.out.printf("Progama ya guay");
+
 
     }
 }
