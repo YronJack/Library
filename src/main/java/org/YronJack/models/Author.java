e7c16b348e191f01726c6b74a42b05a8d06be7ae
Ramix76 <framos-p@student.42barcelona.com>
2025-07-30 14:01:23 +0200

    classes done

--- src/main/java/org/YronJack/models/Author.java
+++ src/main/java/org/YronJack/models/Author.java
@@ -1,4 +1,29 @@
 package org.YronJack.models;
 
 public class Author {
+    private Integer authorId;
+    private String name;
+    private String email;
+    private Book authorBook;
+    int counter = 0;
+
+    public Author(String name, String email, Book authorBook) {
+        this.name = name;
+        this.email = email;
+        this.authorBook = authorBook;
+        this.authorId = counter;
+        counter++;
+    }
+
+    // GETTERS AND SETTERS
+
+    public Integer getAuthorId() { return authorId; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public Book getAuthorBook() { return authorBook; }
+
+    public void setAuthorId(Integer authorId) { this.authorId = authorId; }
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+    public void setAuthorBook(Book authorBook) { this.authorBook = authorBook; }
 }
--- src/main/java/org/YronJack/models/Issue.java
+++ src/main/java/org/YronJack/models/Issue.java
@@ -1,4 +1,33 @@
 package org.YronJack.models;
 
 public class Issue {
+    private Integer issueId;
+    private String issueDate;
+    private String returnDate;
+    private Student issueStudent;
+    private Book issueBook;
+    int counter = 0;
+
+    public Issue(Integer issueId, String issueDate, String returnDate, Student issueStudent, Book issueBook) {
+        this.issueId = counter;
+        this.issueDate = issueDate;
+        this.returnDate = returnDate;
+        this.issueStudent = issueStudent;
+        this.issueBook = issueBook;
+        counter++;
+    }
+
+    // GETTERS AND SETTERS
+
+    public Integer getIssueId() { return issueId; }
+    public String getIssueDate() { return issueDate; }
+    public String getReturnDate() { return returnDate; }
+    public Student getIssueStudent() { return issueStudent; }
+    public Book getIssueBook() { return issueBook; }
+
+    public void setIssueId(Integer issueId) { this.issueId = issueId; }
+    public void setIssueDate(String issueDate) { this.issueDate = issueDate; }
+    public void setReturnDate(String returnDate) { this.returnDate = returnDate; }
+    public void setIssueStudent(Student issueStudent) { this.issueStudent = issueStudent; }
+    public void setIssueBook(Book issueBook) { this.issueBook = issueBook; }
 }
