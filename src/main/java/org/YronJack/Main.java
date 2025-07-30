8f277a9ceedaecbc0e979b2979e00f95cd4d2378
Ramix76 <framos-p@student.42barcelona.com>
2025-07-30 12:16:06 +0200

    book style changes

---
+++ src/main/java/org/YronJack/Main.java
@@ -0,0 +1,10 @@
+package org.YronJack;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Progama ya guay");
+
+    }
+}
---
+++ src/main/java/org/YronJack/models/Student.java
@@ -0,0 +1,17 @@
+package org.YronJack.models;
+
+public class Student {
+    private String usn;
+    private String name;
+
+    public Student(String usn, String name) {
+        this.usn = usn;
+        this.name = name;
+    }
+
+    public String getUsn() { return usn; }
+    public String getName() { return name; }
+
+    public void setUsn(String usn) { this.usn = usn; }
+    public void setName(String name) { this.name = name; }
+}
