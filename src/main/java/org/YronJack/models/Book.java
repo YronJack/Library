1b6a3f4be0b8dfe7e563454204ffdded986c6e1e
aalle <test@email.com>
2025-07-30 12:50:48 +0200

    Push Test 2

--- src/main/java/org/YronJack/models/Book.java
+++ src/main/java/org/YronJack/models/Book.java
@@ -4,7 +4,7 @@ public class Book {
     private String isbn;
     private String title;
     private String category;
-    private int quantity; // test
+    private int quantity;
 
 
     public void Book (String isbn, String title, String category, int quantity ) {
